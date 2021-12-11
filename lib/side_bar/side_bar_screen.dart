7f45340c726609a1274849614d36d3431c2582b7
alsayedabdelbaqeyahmed <alsayedabdelbaqeyahmed1994@gmail.com>
2021-12-12 00:39:14 +0200

    finally

--- lib/side_bar/side_bar_screen.dart
+++ lib/side_bar/side_bar_screen.dart
@@ -17,9 +17,10 @@ class SideBar extends StatelessWidget {
             child: Image.asset("assets/images/logo.png"),
           ),
           ListTileDrawer(
-              title: "Dashbord",
-              svgSrc: "assets/icons/menu_dashbord.svg",
-              press: () {}),
+            title: "Dashbord",
+            svgSrc: "assets/icons/menu_dashbord.svg",
+            press: () {},
+          ),
           ListTileDrawer(
             title: "Transaction",
             svgSrc: "assets/icons/menu_tran.svg",
